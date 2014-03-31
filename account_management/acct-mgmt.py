3766facf4acd40efb7c02cfeb5439e53b687cd11
Nicolas Braun <nbraunhaiti@gmail.com>
2014-03-31 15:42:24 -0400

    test
    test

---
+++ account_management/acct-mgmt.py
@@ -0,0 +1,12 @@
+__author__ = 'nb5kn'
+
+import sys, bcrypt
+
+def changePass(self, password):
+    """If user successfully validates current pass, updates password to newPass"""
+    if is_valid(password):
+        
+
+def getPass(self):
+    """Returns the current user's password"""
+    return this.pass
