74cf00fc58d7dbfb00c7f11d6cbb8e8254b269b6
Nicolas Braun <nbraunhaiti@gmail.com>
2014-03-31 16:04:10 -0400

    Changes to acct-mgmt.py
    Added some comments for direction. Dylan, could you help me out for
    this?

--- account_management/acct-mgmt.py
+++ account_management/acct-mgmt.py
@@ -1,11 +1,13 @@
 __author__ = 'nb5kn'
 
-import sys, bcrypt
+import sys, bcrypt, sqlite3
 
 def changePass(currPass, password):
     """If user successfully validates current pass, updates password to newPass"""
     if is_valid(currPass):
-
+        set_password("user-id", password)
+        return True
+    return False
 
 def is_valid(password):
     """If passed password is valid, returns True"""
@@ -15,3 +17,15 @@ def is_valid(password):
         return True
     else:
         return False
+
+def get_password(username):
+    """For the given username, returns the password from the database"""
+    # use sqlite3 to get password
+
+def set_password(username, password):
+    """Updates the passed user's password"""
+    # encrypt passed parameter
+    # should we first validate currPass? for security measures
+    # update password using sqlite3 and bcrypt
+
+
