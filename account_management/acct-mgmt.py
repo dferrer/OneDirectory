2fbb22ffc8da02a4016cc63b0ccdee0b8678f456
Nicolas Braun <nbraunhaiti@gmail.com>
2014-03-31 15:57:00 -0400

    updated acct_mgmt.py

--- account_management/acct-mgmt.py
+++ account_management/acct-mgmt.py
@@ -2,11 +2,16 @@ __author__ = 'nb5kn'
 
 import sys, bcrypt
 
-def changePass(self, password):
+def changePass(currPass, password):
     """If user successfully validates current pass, updates password to newPass"""
-    if is_valid(password):
-        
+    if is_valid(currPass):
 
-def getPass(self):
-    """Returns the current user's password"""
-    return this.pass
+
+def is_valid(password):
+    """If passed password is valid, returns True"""
+    encoded = password.encode("utf8")
+    hashed = bcrypt.hashpw(encoded, bcrypt.gensalt())
+    if bcrypt.hashpw(encoded, hashed) == hashed:
+        return True
+    else:
+        return False
