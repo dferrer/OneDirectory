7c1553fd176a855dc14c54f5aa7de3d091b30339
Nicolas Braun <nbraunhaiti@gmail.com>
2014-04-27 18:50:51 -0400

    update client.py again

--- client/client.py
+++ client/client.py
@@ -65,15 +65,15 @@ def toggle_autosync(user):
     """Turns autosync on or off for the user."""
     cursor.execute("SELECT auto_sync FROM account WHERE user_id = %s", (user,))
     if cursor.fetchone()[0] == 0:
-	    cursor.execute("UPDATE account SET auto_sync = 1 WHERE user_id = %s", (user,))
+        cursor.execute("UPDATE account SET auto_sync = 1 WHERE user_id = %s", (user,))
         print "Turning on autosync."
-	    return True
+        return True
     elif cursor.fetchone()[0] == 1:
-	    cursor.execute("UPDATE account SET auto_sync = 0 WHERE user_id = %s", (user,))
+        cursor.execute("UPDATE account SET auto_sync = 0 WHERE user_id = %s", (user,))
         print "Turning off autosync."
-	    return True
+        return True
     else:
-	    return False
+        return False
 
 def prompt():
     """Prompts the user for a command."""
